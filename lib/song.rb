671933fe6564bf95192a50e14fcbd9c11abf67be
Zachary Lehmann <zlehmann@gmail.com>
2018-09-26 23:19:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,14 +2,48 @@ class Song
   attr_accesor :name, :artist, :genre
 
   @@count = 0
+  @@genres = []
+  @@artists = []
 
   def initialize(name, artist, genre)
     @@count += 1
-
+    @@genres << genre
+    @@artists << artist
   end
 
   def self.count
     @@count
   end
 
+  def self.genres
+    unique_genres = []
+    @@genres.each do |genre|
+      if unique_genres.include?(genre) != true
+        unique_genres << genre
+      end
+    end
+    unique_genres
+  end
+
+  def self.artists
+    unique_artists = []
+    @@artists.each do |artist|
+      if unique_artists.include?(artist) != true
+        unique_artists << artist
+      end
+    end
+    unique_artists
+  end
+
+  def self.genre_count
+    genre_hash = {}
+    count = 0
+    @@genres.each do |genre|
+      if genre_hash.include?(genre) == false
+        genre_hash[genre] = count
+      else
+        genre_hash[genre] +=1
+      end
+    end
+    genre_hash
 end
